8902a3510fcffc2d84cb2121dcbd57446a4cb3d9
Megan Ghastin <mghastin@gmail.com>
2017-12-13 10:05:38 -0500

    Generalized nba scrapers to one module

---
+++ scraper.py
@@ -0,0 +1,75 @@
+# This module scrapes data from the NBA stats website and saves it to file
+# python scraper.py [link] [output file]
+# nba stats web address to be scraped is first argument
+# second argument passed in the name of the output file (csv) you would like data to be
+# saved to.
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import csv
+import time
+import sys
+
+
+# This function converts an html file with a table and returns two lists:
+# 	1. header row
+#	2. list of rows, each row a list of "cell" strings
+
+def get_data(html):
+	soup = BeautifulSoup(html, 'html.parser')
+	table = soup.find("table")
+	headers = [header.text.encode('utf8')  for header in table.find_all("th")]
+	rows = []
+	for row in table.tbody.find_all("tr"):
+		cells = [cell.text.encode('utf8')  for cell in row.find_all("td")]
+		rows.append(cells)
+	return headers, rows;
+
+# Write to csv file	
+
+def write_table_to_file(filename, headers, rows):
+	with open(filename, 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerow(headers)
+		writer.writerows(rows)
+	f.close()
+	return;
+
+
+try:	
+	WEBSITE = sys.argv[1]
+	FILE_OUT = sys.argv[2]	
+except ValueError:
+	print "No arguments passed. Function needs two arguments- \'python scraper.py [link] [output file]\'"
+	
+
+# Selenium is used to automate web browser clicks and find the HTML for data table on page
+# Website used a lot of js and clicking is needed to go through multiple pages
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get(WEBSITE)
+
+prev_table_html = ""
+data = []
+headers = []
+
+while True:
+	table = driver.find_element_by_tag_name('nba-stat-table') 	# get data table from page
+	html = table.get_attribute('innerHTML')
+	if html == prev_table_html:  # if not another page, stop and write data
+		write_table_to_file(FILE_OUT, headers, data)
+		break
+	headers, rows = get_data(html) 
+	data += rows # add the new table rows to data list
+	prev_table_html = html
+	try:
+		next_page_button = driver.find_element_by_class_name("stats-table-pagination__next")
+		next_page_button.click()
+	except:
+		write_table_to_file(FILE_OUT, headers, data)
+		break
+
+driver.quit()
+print "Player data for current season from stats.nba.com written to " + FILE_OUT
+
+
